c37efb009c1467c18d47ac1a4a31eeab3970a6f8
szkibak.szabolcs <szkibak.szabolcs@BURG-2E05A-02>
2025-10-21 13:20:18 +0200

    Add project files.

---
+++ WebApplication3/Models/Hajo.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication3.Models
+{
+    public class Hajo
+    {
+        public int Azon { get; set; }
+        public string Nev { get; set; }
+        public string Tipus { get; set; }
+        public int Ferohely { get; set; }
+    }
+}
---
+++ WebApplication3/Models/Tura.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication3.Models
+{
+    public class Tura
+    {
+        public int HajoAzon { get; set; }
+        public int Nap { get; set; }
+        public int Szemely { get; set; }
+    }
+}
