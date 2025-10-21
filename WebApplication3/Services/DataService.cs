163b24cccdca1685982647bf740f31349d3e1af2
szkibak.szabolcs <szkibak.szabolcs@BURG-2E05A-02>
2025-10-21 13:34:03 +0200

    Data Service (kesz)

--- WebApplication3/Services/DataService.cs
+++ WebApplication3/Services/DataService.cs
@@ -1,6 +1,49 @@
-﻿namespace WebApplication3.Services
+﻿using WebApplication3.Models;
+
+namespace WebApplication3.Services
 {
     public class DataService
     {
+        public List<Hajo> Hajok { get; private set; }
+        public List<Tura> Turak { get; private set; }
+
+        public DataService() 
+        {
+            Hajok = BeolvasHajok("Data/hajo.txt");
+            Turak = BeolvasTurak("Data/tura.txt");
+        }
+
+        private List<Hajo> BeolvasHajok(string path)
+        {
+            return File.ReadAllLines(path)
+                .Skip(1)
+                .Select(sor =>
+                {
+                    var d = sor.Split('\t');
+                    return new Hajo
+                    {
+                        Azon = int.Parse(d[0]),
+                        Nev = d[1],
+                        Tipus = d[2],
+                        Ferohely = int.Parse(d[3])
+                    };
+                }).ToList();
+        }
+
+        private List<Tura> BeolvasTurak(string path)
+        {
+            return File.ReadAllLines(path)
+                .Skip(1)
+                .Select(sor =>
+                {
+                    var d = sor.Split('\t');
+                    return new Tura
+                    {
+                        HajoAzon = int.Parse(d[0]),
+                        Nap = int.Parse(d[1]),
+                        Szemely = int.Parse(d[2])
+                    };
+                }).ToList();
+        }
     }
 }
