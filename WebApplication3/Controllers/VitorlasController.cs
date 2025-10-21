b52d0e24e7130e5742eb86b10fb6d62e85cba7fd
szkibak.szabolcs <szkibak.szabolcs@BURG-2E05A-02>
2025-10-21 13:44:04 +0200

    kesz de lehet hiba

--- WebApplication3/Controllers/VitorlasController.cs
+++ WebApplication3/Controllers/VitorlasController.cs
@@ -1,6 +1,51 @@
-﻿namespace WebApplication3.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication3.Services;
+
+namespace WebApplication3.Controllers
 {
-    public class VitorlasController
+    [ApiController]
+    [Route("api/[controller]")]
+    public class VitorlasController : ControllerBase
     {
+        private readonly DataService _data;
+
+        public VitorlasController()
+        {
+            _data = new DataService();
+        }
+
+        [HttpGet("hajok")]
+        public IActionResult GetHajok() => Ok(_data.Hajok);
+
+        [HttpGet("turak")]
+        public IActionResult GetTurak() => Ok(_data.Turak);
+
+        [HttpGet("legtobbtura")]
+        public IActionResult GetLegtobbetHasznaltHajo()
+        {
+            var legtobb = _data.Turak
+                .GroupBy(t => t.HajoAzon)
+                .OrderByDescending(g => g.Count())
+                .First();
+
+            var hajo = _data.Hajok.First(h => h.Azon == legtobb.Key);
+
+            return Ok(new
+            {
+                Hajo = hajo.Nev,
+                Tipus = hajo.Tipus,
+                TurakSzama = legtobb.Count()
+            });
+        }
+
+        [HttpGet("resztvevok/{nap}")]
+        public IActionResult GetResztvevokNapSzerint(int nap)
+        {
+            int osszes = _data.Turak
+                .Where(t => t.Nap == nap)
+                .Sum(t => t.Szemely);
+
+            return Ok(new { Nap = nap, OsszesResztvevo = osszes });
+        }
     }
 }
--- WebApplication3/Program.cs
+++ WebApplication3/Program.cs
@@ -1,15 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -17,9 +18,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
